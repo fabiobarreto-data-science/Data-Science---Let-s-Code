a59b2872d8d8771ccdc14add63af7bb67723d5f3
fabiobarreto-data-science <romaobarreto@hotmail.com>
2021-07-07 15:35:58 -0300

    Revisando Python
    Input e Output

---
+++ Python-01.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+nota01 = float(input("1º NOTA: "))
+nota02 = float(input("2º NOTA: "))
+nota03 = float(input("3º NOTA: "))
+nota04 = float(input("4º NOTA: "))
+media = (nota01+nota02+nota03+nota04)/4
+print(f"A média será de {media:.2f}")
