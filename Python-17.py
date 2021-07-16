791dfde13ecc8f79c8b3f7efd512ae18c3184bd6
fabiobarreto-data-science <romaobarreto@hotmail.com>
2021-07-16 15:41:54 -0300

    Estrutura while e for
    Revisando estrutura de repetição

--- Python-17.py
+++ Python-17.py
@@ -3,3 +3,10 @@ Enunciado
 Informe, de forma decrescente todos os pares entre N (número fornecido pelo usuário) e -N.
 """
 
+N = int(input("Digite um valor inteiro: "))
+
+for numero in range(0, N, 2):
+    print(f"{numero}, ", end="")
+
+print()
+print(f"{-1*N}")
