7dcd5db7d4ecdfe74d885e2ac1615ae3fe3c83f8
fabiobarreto-data-science <romaobarreto@hotmail.com>
2021-10-04 17:16:29 -0300

    Aprendendo Pandas

---
+++ Exercicio_class.py
@@ -0,0 +1,10 @@
+"""
+Enunciado
+Printe os elementos dos seguintes dois vetores usando apenas um for:
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+Depois, faça o mesmo usando numpy (usando a função nditer) e deixe um comentário sobre a diferença dos dois métodos.
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+"""
